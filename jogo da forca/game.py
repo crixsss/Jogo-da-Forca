6be10326edc495938abf5ba0a773811950c3ca20
crixsss <83135115+crixsss@users.noreply.github.com>
2022-06-15 19:50:23 -0300

    Update game.py

--- jogo da forca/game.py
+++ jogo da forca/game.py
@@ -11,85 +11,85 @@ while True:
 competidor = input('Competidor: ')
 clean()
 palavra = input('digite a palavra: ')
-arquivoPL = open('players.txt', 'w')
-arquivoPL.write(f'DESAFIANTE > {desafiante}')
-arquivoPL.write('\n')
-arquivoPL.write(F'COMPETIDOR > {competidor}')
-arquivoPL.write('\n')
-arquivoPL.write(f'PALAVRA > {palavra}')
-arquivoPL.write('\n')
-arquivoPL.close()
-
-
-def jogo():
    
-    dica1 = input('Digite a dica 1: ')
-    dica2 = input('Digite a dica 2: ')
-    dica3 = input('Digite a dica 3: ')
+dica1 = input('Digite a dica 1: ')
+dica2 = input('Digite a dica 2: ')
+dica3 = input('Digite a dica 3: ')
+digitados = []
+chance = 5
 
-    digitados = []
-    chance = 5
-
-    while True:
-        print('(1) JOGAR')
-        print('(2) DICA')
-        print('(3) CHUTAR PALAVRA')
-        lol = input()
-        if lol =='1':
-            letra = input('Competidor, digite uma letra: ')
-            
-            if len(letra) > 1:
-                print('Não digite mais que uma letra!')
-                continue
-            
-            digitados.append(letra)
-            secreto_temp = ''
-            for letra_secre in palavra:
-                if letra_secre in digitados:
-                    secreto_temp += letra_secre
-                else:
-                    secreto_temp += '*'
-
-            if secreto_temp == palavra:
-                print('Competidor venceu!')
-                break
+while True:
+    print('(1) JOGAR')
+    print('(2) DICA')
+    print('(3) CHUTAR PALAVRA')
+    lol = input()
+    if lol =='1':
+        letra = input('Competidor, digite uma letra: ')
+        
+        if len(letra) > 1:
+            print('Não digite mais que uma letra!')
+            continue
+        
+        digitados.append(letra)
+        secreto_temp = ''
+        for letra_secre in palavra:
+            if letra_secre in digitados:
+                secreto_temp += letra_secre
             else:
-                print(f'Palavra secreta: {secreto_temp}')
-            
-            if letra not in palavra:
-                chance -= 1
-            
-            if chance <= 0:
-                print('Desafiante venceu!')
-                break
-            print(f'O competidor ainda tem {chance} chances. ')
-            print()
-        elif lol == '2':
-            print('Escolha qual dica você quer:')
-            print('(1) DICA 01')
-            print('(2) DICA 02')
-            print('(3) DICA 03')
-            dicaEscolida = input()
-            if dicaEscolida == '1':
-                print(dica1)
-            elif dicaEscolida == '2':
-                print(dica2)
-            elif dicaEscolida == '3':
-                print(dica3)
-            time.sleep(4)
-        elif lol == '3':
-            chuteR = input('Digite seu chute: ')
-            if chuteR == palavra:
-                print('Competidor venceu!')  
-                break 
-            elif chuteR != palavra:
-                print('Desafiante venceu!')
-                break
-jogo()
+                secreto_temp += '*'
 
+        if secreto_temp == palavra:
+            vencedor = (f"Desafiante perdedor {desafiante}. Comepetidor vencedor {competidor}.\n")
+            break
+        else:
+            print(f'Palavra secreta: {secreto_temp}')
+        
+        if letra not in palavra:
+            chance -= 1
+        
+        if chance <= 0:
+            vencedor = (f"Desafiante vencedor {desafiante}. Comepetidor perdedor {competidor}.\n")
+            break
+        print(f'O competidor ainda tem {chance} chances. ')
+        print()
+    elif lol == '2':
+        print('Escolha qual dica você quer:')
+        print('(1) DICA 01')
+        print('(2) DICA 02')
+        print('(3) DICA 03')
+        dicaEscolida = input()
+        if dicaEscolida == '1':
+            print(dica1)
+        elif dicaEscolida == '2':
+            print(dica2)
+        elif dicaEscolida == '3':
+            print(dica3)
+        time.sleep(4)
+    elif lol == '3':
+        chuteR = input('Digite seu chute: ')
+        if chuteR == palavra:
+            vencedor = (f"Desafiante perdedor {desafiante}. Comepetidor vencedor {competidor}.\n")  
+            break 
+        elif chuteR != palavra:
+            chance -= 1
+            continue  
+while True:
+     clean()
+     try:
+        print ("\n")
+        arquivo = open("arquivo.txt", "a")
+        arquivo.write(vencedor + "\n")
+        arquivo.close()
 
-
-
-
-
-    
+        arquivo = open("arquivo.txt", "r")
+        clean()
+        print("Histórico de partidas:\n")
+        for linha in arquivo:
+            linha = linha.rstrip()
+            print(linha)
+        arquivo.close()
+        print()
+        break
+     except:
+        arquivo = open("arquivo.txt", "w")
+        arquivo.close()
